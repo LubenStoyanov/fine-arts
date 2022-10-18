86c7e1ef0c37be7350b2326594d94a633963db72
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2022-10-18 09:54:26 +0200

    Swap card

--- src/routes/Literature.jsx
+++ src/routes/Literature.jsx
@@ -13,25 +13,38 @@ export default function Literature() {
   console.log(books);
 
   const mappedBooks = books.map((book) => (
-    <a className="link " href={book.fields.link} target="_blank">
-      <div
-        className="card card-compact w-60 sm:w-40 bg-base-100 shadow-xl"
-        key={book.sys.id}
-      >
-        <figure>
-          <img
-            src={book.fields.cover.fields.file.url}
-            alt={`Book cover of ${book.fields.title} by ${book.fields.author}`}
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="text-center">
-            {book.fields.title} <br />
-            by {book.fields.author}
-          </h2>
+    <div>
+      <label className="swap swap-flip text-9xl">
+        <input type="checkbox" />
+        <div className="swap-on">
+          <div
+            className="card card-compact w-60 sm:w-40 bg-base-100 shadow-xl"
+            key={book.sys.id}
+          >
+            😈
+          </div>
         </div>
-      </div>
-    </a>
+        {/* <a className="link " href={book.fields.link} target="_blank"> */}
+        <div
+          className="card card-compact w-60 sm:w-40 bg-base-100 shadow-xl swap-off"
+          key={book.sys.id}
+        >
+          <figure>
+            <img
+              src={book.fields.cover.fields.file.url}
+              alt={`Book cover of ${book.fields.title} by ${book.fields.author}`}
+            />
+          </figure>
+          {/* <div className="card-body">
+            <h2 className="text-center">
+              {book.fields.title} <br />
+              by {book.fields.author}
+            </h2>
+          </div> */}
+        </div>
+        {/* </a> */}
+      </label>
+    </div>
   ));
 
   return (

ae87be20bf224591fdbf8919f5ab4423a6f46acc
fabian-elmar <fabsiban09@gmail.com>
2022-10-18 14:49:33 +0200

    wikibotto

--- src/routes/Art.jsx
+++ src/routes/Art.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { Link } from "react-router-dom/dist";
 import { useLoaderData } from "react-router-dom";
-import debounce from "lodash/debounce";
+import shuffle from "lodash/shuffle";
 import contentful from "../data/contentful";
+import { flip } from "lodash";
 
 export async function loader() {
   const { getArt } = contentful();
@@ -12,33 +13,39 @@ export async function loader() {
 
 export default function Art() {
   const { art } = useLoaderData();
+  const sart = shuffle(art)
+  console.log(sart)
   console.log("art", art);
+  const flip = false
+
 
   return (
-    <div className="">
-      {art.map((a) => (
+    <div className="space-y-4">
+      {sart.map((a) => (
         <div key={a.sys.id} className="card lg:card-side bg-base-100 shadow-xl">
           <figure>
+
             <img
-              onMouseEnter={() => console.log(a.fields.link)}
+              /* onMouseEnter={() => flip = true}  
+                onMouseLeave={() => flip = false} */
               src={a.fields.artworks[0].fields.file.url}
               alt="Album"
               width="480px"
+
             />
+
+
           </figure>
           <div className="card-body">
             <h2 className="card-title text-red-800 justify-center">
               {a.fields.title}
             </h2>
-            <h3 className="card-title justify-center">{a.fields.artist}</h3>
+            <h3 className="card-title justify-center">{a.fields.artist} {!a.fields.alive ? <span>♱</span> : <></>}
+            </h3>
             <p>{a.fields.description}</p>
-            <div className="card-actions justify-end">
-              <button
-                onMouseEnter={() => setToggle(true)}
-                className="btn btn-primary"
-              >
-                Listen
-              </button>
+            <div className="card-actions justify-center">
+              <a target="blank" href={a.fields.link} className="btn btn-primary">Atrist Info</a>
+
             </div>
           </div>
         </div>
